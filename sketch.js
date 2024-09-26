623b1e8505cab94defad3017186f8652b2e287b2
Gabi <160881036+GabrieliGroth@users.noreply.github.com>
2024-09-26 09:24:08 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,20 @@
+//ficção cientifica,terror,aventura
+
+//sombra e ossos,16,fantasia,aventura
+//manual de assasinato para boas garatas,16,investigação,uspence
+//sobrenatural,16,fantasia,suspence,misterio
+//os originais,16,fantasia
+//grey's anatomy,16,drama
+
+//umbrella,18,fantaia,violência
+//os jovens titans,18,fantasia,violência
+//the witcher,18,fantasia,terror,supence
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(250)
+  
+}

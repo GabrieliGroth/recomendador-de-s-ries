b9df129ab02c65c6083e78be7404fb87ec599e1d
Gabi <160881036+GabrieliGroth@users.noreply.github.com>
2024-10-24 11:25:53 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,8 +2,8 @@ let campoIdade;
 let campoFantasia;
 let campoAventura;
 
-function setup(){
-  createCanvas(800, 400);
+function setup() {
+  createCanvas(900, 600);
   createElement("h2", "Recomendador de series");
   createSpan("Sua idade:");
   campoIdade = createInput("5");
@@ -12,18 +12,42 @@ function setup(){
 }
 
 function draw() {
-  background("white");
+  background("#9C27B0");
   let idade = campoIdade.value();
   let gostaDeFantasia = campoFantasia.checked();
   let gostaDeAventura = campoAventura.checked();
-  let recomendacao = geraRecomendacao(idade, gostoDeFantasia, gostaDeAventura);
-  
-  fill(color(76, 0, 115));
-  textAling(CENTER, CENTER);
+  let recomendacao = geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura);
+
+  fill(color(78, 0, 116));
+  textAlign(CENTER, CENTER);
   textSize(38);
-  text(recomendacoa, width / 2, height / 2);
+  text(recomendacao, width / 2, height / 2);
 }
 
-function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura)
-{
-  if (idade )
+function geraRecomendacao(idade, gostaDeFantasia, gostaDeAventura) {
+  if (idade >= 14) {
+    if (idade >= 16) {
+      return "sobrenatural";
+    } else {
+      if (idade >= 14) {
+        if(gostaDeFantasia || gostaDeAventura) {
+          return "sombrs e ossos";          
+        } else{
+         return "diario de um vampiro";
+        }
+      } else {
+        if (gostaDeFantasia) {
+          return "os originais";
+        } else {
+          return "os jovens titans";
+        }
+      }
+    }
+  } else {
+    if (gostaDeFantasia) {
+      return "manual de assasinato para boas garotas";
+    } else {
+      return "grey's anatomy";
+    }
+  }
+}
